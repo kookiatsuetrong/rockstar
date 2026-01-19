d7f329fc26d4deb99f8787841792ff9ab8499728
Kookiat Suetrong <kookiatsuetrong@gmail.com>
2026-01-20 01:10:52 +0700

    Fixed UTF8 input

--- code/Context.java
+++ code/Context.java
@@ -1,6 +1,8 @@
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import org.json.JSONObject;
 
 public class Context {
@@ -11,12 +13,18 @@ public class Context {
 		return request.getParameter(key);
 	}
 	
+	public JSONObject getJSON() {
+		return getJson();
+	}
+	
 	public JSONObject getJson() {
 		String buffer = "";
 		try {
-			var input = request.getInputStream();
+			var input  = request.getInputStream();
+			var reader = new InputStreamReader(input, 
+								StandardCharsets.UTF_8);
 			while (true) {
-				int k = input.read();
+				int k = reader.read();
 				if (k == -1) break;
 				buffer += (char)k;
 			}
